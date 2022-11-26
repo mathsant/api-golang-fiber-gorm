606849a693dd5f6dc578d8a5745b3476760d3314
mathsant <mathsant_99@hotmail.com>
2022-11-26 14:00:02 -0300

    adjusting assets for project

--- routes/index.route.go
+++ routes/index.route.go
@@ -1,12 +1,14 @@
 package routes
 
 import (
+	"mathsant/web-service-fiber/config"
 	"mathsant/web-service-fiber/handler"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func RouteInit(r *fiber.App) {
+	r.Static("/public", config.ProjectRootPath+"/public/asset")
 	r.Get("/users", handler.UserHandlerGetAll)
 	r.Get("/user/:id", handler.UserHandleGetById)
 	r.Post("/user", handler.UserHandlerCreate)
