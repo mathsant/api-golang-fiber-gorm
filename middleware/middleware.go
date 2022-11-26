e1c8e4936fce9ab3c463bb9bb801648d59a33e54
mathsant <mathsant_99@hotmail.com>
2022-11-26 15:25:04 -0300

    adding middleware of auth

---
+++ middleware/middleware.go
@@ -0,0 +1,13 @@
+package middleware
+
+import "github.com/gofiber/fiber/v2"
+
+func Auth(ctx *fiber.Ctx) error {
+	token := ctx.Get("x-token")
+	if token != "secret" {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	return ctx.Next()
+}
--- routes/index.route.go
+++ routes/index.route.go
@@ -3,23 +3,14 @@ package routes
 import (
 	"mathsant/web-service-fiber/config"
 	"mathsant/web-service-fiber/handler"
+	"mathsant/web-service-fiber/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func middleware(ctx *fiber.Ctx) error {
-	token := ctx.Get("x-token")
-	if token != "secret" {
-		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"message": "Unauthenticated",
-		})
-	}
-	return ctx.Next()
-}
-
 func RouteInit(r *fiber.App) {
 	r.Static("/public", config.ProjectRootPath+"/public/asset")
-	r.Get("/users", middleware, handler.UserHandlerGetAll)
+	r.Get("/users", middleware.Auth, handler.UserHandlerGetAll)
 	r.Get("/user/:id", handler.UserHandleGetById)
 	r.Post("/user", handler.UserHandlerCreate)
 	r.Put("/user/:id", handler.UserHandlerUpdate)
