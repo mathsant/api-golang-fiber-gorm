ac962241ac3edbd8522cbe132f99688ebbf0443b
mathsant <mathsant_99@hotmail.com>
2022-11-26 14:10:16 -0300

    adding middleware

--- routes/index.route.go
+++ routes/index.route.go
@@ -7,9 +7,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func middleware(ctx *fiber.Ctx) error {
+	token := ctx.Get("x-token")
+	if token != "secret" {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	return ctx.Next()
+}
+
 func RouteInit(r *fiber.App) {
 	r.Static("/public", config.ProjectRootPath+"/public/asset")
-	r.Get("/users", handler.UserHandlerGetAll)
+	r.Get("/users", middleware, handler.UserHandlerGetAll)
 	r.Get("/user/:id", handler.UserHandleGetById)
 	r.Post("/user", handler.UserHandlerCreate)
 	r.Put("/user/:id", handler.UserHandlerUpdate)
